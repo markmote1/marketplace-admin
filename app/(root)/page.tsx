ba35bf0fa289d2ade3003dcb18e727caf0db36d4
Mark Mote <137040517+markmote1@users.noreply.github.com>
2025-01-03 17:19:24 +0300

    marketplace start

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Page: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the Marketplace Admin</h1>
+            <p>This is the main page of the admin panel.</p>
+        </div>
+    );
+};
+
+export default Page;
